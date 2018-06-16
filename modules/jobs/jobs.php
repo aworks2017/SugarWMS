83eaa6a79017499d8938de597c27ffa9a3028c14
Joe Badrov <joe.badrov@gmail.com>
2018-06-15 22:33:02 -0500

    Fixed export error in jobs module

--- modules/jobs/jobs.php
+++ modules/jobs/jobs.php
@@ -2,31 +2,31 @@
 /*********************************************************************************
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
  * Free Software Foundation with the addition of the following permission added
  * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
  * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
  * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with
  * this program; if not, see http://www.gnu.org/licenses or write to the Free
  * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301 USA.
- * 
+ *
  * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
  * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU Affero General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
  * SugarCRM" logo. If the display of the logo is not reasonably feasible for
@@ -39,23 +39,17 @@
  */
 require_once('modules/jobs/jobs_sugar.php');
 class jobs extends jobs_sugar {
-	
-	function jobs(){	
-		parent::jobs_sugar();
-	}
-	function create_new_list_query($order_by, $where,$filter=array(),$params=array(), $show_deleted = 0,$join_type='', $return_array = false,$parentbean=null, $singleSelect = false)
-	{
-		$return_array =  parent::create_new_list_query($order_by, $where, $filter, $params, $show_deleted, $join_type, $return_array, $parentbean, $singleSelect);
-		if(isset($_REQUEST['account_name_advanced']) && !empty($_REQUEST['account_name_advanced'])){
-			$result=preg_match_all('/(?<=(accounts))(\s\w*)/',$return_array['from'],$matches);
-			$return_array['where'] = str_replace('a.account_name', $matches[0][0].'.name', $return_array['where']);
+
+	function jobs(){
+			parent::jobs_sugar();
 		}
-		if($order_by=='group_id ASC' || $order_by=='group_id DESC' || str_replace(" ",$order_by)=='group_id'){
-			$return_array['order_by'] .= ', jobs.order_number asc, jobs.priority_ratio asc';
-		}else{
-			$return_array['order_by'] .= ', jobs.group_id asc , jobs.order_number asc, jobs.priority_ratio asc';			
-		}
-		return $return_array;
+		function create_new_list_query($order_by, $where,$filter=array(),$params=array(), $show_deleted = 0,$join_type='', $return_array = false,$parentbean=null, $singleSelect = false)
+		{
+			$return_array =  parent::create_new_list_query($order_by, $where, $filter, $params, $show_deleted, $join_type, $return_array, $parentbean, $singleSelect);
+			if(isset($_REQUEST['account_name_advanced']) && !empty($_REQUEST['account_name_advanced'])){
+				$return_array['where'] = str_replace('a.account_name', 'jt2.name', $return_array['where']);
+			}
+			return $return_array;
 	}
 }
 ?>
